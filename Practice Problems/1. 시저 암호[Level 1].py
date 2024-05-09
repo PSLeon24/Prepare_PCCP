77c93a9083454e10f6248776f213b99b8aaa1b44
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2024-05-09 14:21:28 +0900

    Create 1. 시저 암호[Level 1].py

---
+++ Practice Problems/1. 시저 암호[Level 1].py
@@ -0,0 +1,13 @@
+def solution(s, n):
+    answer = ""
+    for i in s:
+        if i.isalpha():
+            if i.islower():
+                answer += chr((ord(i) - ord("a") + n) % 26 + ord("a"))
+            else:
+                answer += chr((ord(i) - ord("A") + n) % 26 + ord("A"))
+        else:
+            answer += " "
+
+    print(answer)
+    return answer
